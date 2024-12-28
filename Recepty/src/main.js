ec86f3173d4cd3e04ae06366c659bd4ffea06055
kowyzu <kowyzu@gmail.com>
2024-12-28 14:47:21 +0100

    structure of main page in progress

--- Recepty/src/main.js
+++ Recepty/src/main.js
@@ -22,14 +22,56 @@ async function fetchData() {
   }
 
   console.log("Fetched data:", data);
-  receiptsData = data;
+  recipesData = data;
+}
+
+function displayReceipt(recipesData) {
+  const recipesPlaceHolder = document.querySelector(".recipesList");
+  console.log(recipesPlaceHolder);
+
+  recipesData.forEach((recipe) => {
+    let newRecipe = document.createElement("div");
+    newRecipe.classList.add("recipe");
+
+    newRecipe.innerHTML = `
+      <img src="./src/recipes_imgs/recipe_${recipe.id}.webp" class="card-img-top" alt="${recipe.title}">
+            
+      <div class="card-body">
+
+        <h5 class="recipeName" class="card-title">
+          ${recipe.title}
+        </h5>
+
+          <div class="recipeDetails">
+
+            <p class="recipeCategory">
+              ${recipe.category}
+            </p>
+
+            <p class="recipeTime">
+              ${recipe.time} 
+            </p>
+
+
+          </div>
+
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+
+      </div>
+
+    `;
+    console.log(newRecipe);
+    recipesPlaceHolder.appendChild(newRecipe);
+  });
 }
 
 //////////////////
 //Main Programm //
 /////////////////
 
-let receiptsData = [];
+let recipesData = [];
 await fetchData();
 
-console.log(receiptsData);
+console.log(recipesData);
+
+displayReceipt(recipesData);
